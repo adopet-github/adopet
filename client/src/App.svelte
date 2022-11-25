097c4e4c0f388d34a11b77da3d025dd273619a9f
chetheaker <che.theaker@gmail.com>
2022-11-25 12:30:28 +0100

    feat: router: basic routes set up

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,47 +1,18 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
-  import Counter from './lib/Counter.svelte';
+  import { Router, Route } from 'svelte-navigator';
+  import Nav from './Components/Nav.svelte';
+  import Home from './Pages/Home.svelte';
+  import Login from './Pages/Login.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+  <Router>
+    <Nav />
+    <Route path="/">
+      <Home />
+    </Route>
+    <Route path="login">
+      <Login />
+    </Route>
+  </Router>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ client/src/Components/Nav.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Link } from 'svelte-navigator';
+</script>
+
+<nav>
+  <Link to="/">HOME</Link>
+  <Link to="/login">LOGIN</Link>
+</nav>
