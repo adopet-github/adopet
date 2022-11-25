9dcfe84d0c67302c257b9b2954538b0858bb0102
chetheaker <che.theaker@gmail.com>
2022-11-25 14:38:23 +0100

    feat: login: add base functionality

---
+++ client/src/Components/Inputs/EmailInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let value = '';
+  let input;
+</script>
+
+<input bind:this={input} type="text" placeholder="Email" bind:value />
+
+<style>
+  input {
+    padding: 1rem;
+    border-radius: 10px;
+    width: 50%;
+  }
+
+  input:focus {
+    outline-offset: 1px;
+    outline: solid 2px white;
+  }
+</style>
