2e174f7bfa8cd9f0759509739f2bc5a91b7fd86c
Alex <alexander.crump11@gmail.com>
2022-11-25 20:00:19 +0100

    [feat]:[dashShelter]:Started dashShelter componenet

---
+++ client/src/Pages/DashShelter.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div class="main-container">
+  <h2>Dashboard</h2>
+  <div class="grid-container" />
+</div>
+
+<style>
+  .main-container {
+    display: flex;
+    height: 92vh;
+    background-color: rgb(255, 255, 255, 0.1);
+    /* background-image: url('../assets/SVGs/red_blob_grad.svg'),
+      url('../assets/SVGs/grey_blob_grad.svg'),
+      url('../assets/SVGs/cream_blob_grad.svg'); */
+    background-blend-mode: multiply, screen;
+    background-repeat: no-repeat;
+    background-position: 100% 100%, 0% 0%, 100% 50%;
+    background-size: 100%, 100%, 100%;
+  }
+
+  h2 {
+    color: var(--red);
+    padding: 1%;
+  }
+</style>
