00d84b0f0d139c8f6e75a58db7adde74a85bdf19
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-12-02 17:50:41 +0100

    [fix]:[cors]:cors time

--- server/app/app.ts
+++ server/app/app.ts
@@ -39,7 +39,7 @@ app.use('/', (req, res) => {
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: 'http://localhost:5173' } });
 
 (async function bootstrap() {
   await sequelize.sync();
